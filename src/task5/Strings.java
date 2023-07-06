0f625f309cadc469a1f98edaf5c203ac0bb27425
Elena Rudenko <elru0121@netcracker.com>
2023-07-06 18:15:52 +0300

    Lab 5 changeWord(), isPalindrome(), findLongest() are refactored

--- src/task5/Strings.java
+++ src/task5/Strings.java
@@ -6,56 +6,42 @@ public class Strings {
 
     public static void main(String[] args) {
 
-        String text = "нерадивых собратьев и спас их жизни бяка";
+        String text = "нерадивых собратьев и спас их жизни любопытствующих бяка";
         String text2 = "Жилкос себе дед да баба, у них была курочка Рябакос; кос снесла под полом яичко - пестро, вострокос, костяно, мудрено!";
 //        findLongest(text);
 
-//        isPalindrome("яблоко", "yколбя");
+        isPalindrome("яблоко");
 //        changeWord(text);
 //        substringCount(text2, "кос");
-        invertWords(text);
+//        invertWords(text);
     }
 
     public static String findLongest(String text) {
         String[] words = text.split(" ");
         String maxLength = "";
-        for (int i = 0; i < words.length; i++) {
-            for (int j = 0; j < words.length; j++) {
-                if (words[i].length() < words[j].length()) {
-                    String aj = words[j];
-                    String ai = words[i];
-
-                    words[j] = ai;
-                    words[i] = aj;
-                }
-            }
-        }
-        maxLength = words[words.length - 1];
-        for (int j = words.length - 1; j > 0; j--) {
-            if (words[j].length() == words[j - 1].length()) {
-                maxLength = maxLength.concat(", " + words[j - 1]);
+        for (String word : words) {
+            if(word.length() > maxLength.length()) {
+                maxLength = word;
             }
         }
         System.out.println("Самое длинное слово в тексте: " + maxLength);
         return maxLength;
     }
 
-    public static boolean isPalindrome(String str1, String str2) {
+    public static boolean isPalindrome(String word) {
         boolean result = false;
-        char[] originWord = str1.toCharArray();
-        String revertedString = "";
-        for (int j = originWord.length - 1; j >= 0; j--) {
-            revertedString = revertedString + originWord[j];
-        }
-        if (str2.equals(revertedString)) {
+        StringBuilder builder = new StringBuilder();
+        builder.append(word);
+        String reversedString = builder.reverse().toString();
+        if(word.equals(reversedString)){
             result = true;
         }
         System.out.println(result);
         return result;
     }
 
-    public static void changeWord(String text) {
-        String newText = text.replaceAll("бяка", "[вырезано цензурой]");
+    public static void changeWord(String originText, String wrongWord, String change) {
+        String newText = originText.replaceAll(wrongWord, change);
         System.out.println(newText);
     }
 
