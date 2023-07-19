c10bb091005c4a91c99f05a5e7f69fbedb800529
Elena Rudenko <elru0121@netcracker.com>
2023-07-19 14:02:19 +0300

    Lab 10 refactored

--- src/task10/FilesIO.java
+++ src/task10/FilesIO.java
@@ -8,8 +8,18 @@ import java.util.List;
 public class FilesIO {
 
     public static void main(String[] args) {
+        String pathToWrite1 = "files/fileToWrite.txt";
+        String pathToWrite2 = "files/newFileToWrite.txt";
+
+        String pathToRead = "files/texts.txt";
+        String pathInputFile1 = "files/inputFile1.txt";
+        String pathInputFile2 = "files/inputFile2.txt";
+
+        String text = "I think the real answer is that it completely depends on what your inputs look like.";
+
+
 //        stringsList();
-        writeToFile("I think the real answer is that it completely depends on what your inputs look like. I created a JsFiddle to try a bunch of these and a couple of my own against various inputs. No matter how I look at the results, I see no clear winner.");
+        writeToFile(text, pathToWrite1);
 //        writeFromTwoFiles();
 //       changesInText();
 //        String str = "The story of 10?//]]-= beautiful Snow White, the seven dwarfs and the evil Queen.";
@@ -17,8 +27,8 @@ public class FilesIO {
 //        System.out.println(newStr);
     }
 
-    public static List<String> stringsList(ArrayList<String> strArr) {
-        try (BufferedReader reader = new BufferedReader(new FileReader("files/texts.txt"))) {
+    public static List<String> stringsList(ArrayList<String> strArr, String path) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
             String input;
             while ((input = reader.readLine()) != null) {
                 String[] strings = input.split(" ");
@@ -33,9 +43,9 @@ public class FilesIO {
         return strArr;
     }
 
-    public static void writeToFile(String inputText) {
+    public static void writeToFile(String inputText, String path) {
         byte[] inputBytes = inputText.getBytes(StandardCharsets.UTF_8);
-        try (OutputStream os = new FileOutputStream("files/fileToWrite.txt") {
+        try (OutputStream os = new FileOutputStream(path) {
         }) {
             os.write(inputBytes);
         } catch (IOException e) {
@@ -43,9 +53,9 @@ public class FilesIO {
         }
     }
 
-    public static void writeFromTwoFiles() {
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter("files/newFileToWrite.txt", true));
-             BufferedReader reader1 = new BufferedReader(new FileReader("files/inputFile1.txt")); BufferedReader reader2 = new BufferedReader(new FileReader("files/inputFile2.txt"))) {
+    public static void writeFromTwoFiles(String inputPath1, String inputPath2, String outputPath) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(outputPath, true));
+             BufferedReader reader1 = new BufferedReader(new FileReader(inputPath1)); BufferedReader reader2 = new BufferedReader(new FileReader(inputPath2))) {
             String input1;
             while ((input1 = reader1.readLine()) != null) {
                 writer.write(input1);
@@ -60,8 +70,8 @@ public class FilesIO {
         }
     }
 
-    public static void changesInText() {
-        try (BufferedReader reader = new BufferedReader(new FileReader("files/texts.txt")); BufferedWriter writer = new BufferedWriter(new FileWriter("files/fileToWrite.txt"))) {
+    public static void changesInText(String inputPath, String outputPath) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(inputPath)); BufferedWriter writer = new BufferedWriter(new FileWriter(outputPath))) {
             String input;
             while ((input = reader.readLine()) != null) {
                 System.out.println(input);
